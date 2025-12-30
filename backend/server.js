c8bf7b68970dd584330995aa754319a4de30b84a
Prachi-Gupta456 <rg019436@gmail.com>
2025-12-30 15:52:29 +0530

    updtaed url

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: "https://live-incident-app-xw3y.vercel.app",
+    origin: "http://localhost:5173",
     credentials: true
 }))
 
